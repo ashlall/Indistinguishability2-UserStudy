06f0b32c59e74b03b0d65e17e63afaa7e1e190be
chloec1 <122915643+chloec1@users.noreply.github.com>
2024-06-17 16:08:00 -0400

    Updated display_points_v2, added default boolean to count_slopes
    - Test cases for display_points_v2, min_slope, and count_slopes passing!
    - Added default boolean argument to count_slopes
    - Corrected counting in display_points_v2

--- utility.h
+++ utility.h
@@ -22,7 +22,7 @@ using namespace std;
 SLOPE_TYPE              compute_slope           (point_t* p1, point_t* p2);
 SLOPE_TYPE              min_slope               (point_set_t* P);
 int                     count_inversions        (vector<int>& arr);
-int                     count_slopes            (point_set_t* P, double alpha, double beta);
+int                     count_slopes            (point_set_t* P, double alpha, double beta, bool adjust = false);
 point_t**               display_points_v2       (point_set_t* P, int s, double alpha, double beta, int num_iterations);
 COORD_TYPE              breakpoint_one_round    (point_set_t* P, int s, double alpha, double beta);
 point_t*                breakpoint              (point_set_t* P, point_t* u, int s, int maxRound);
